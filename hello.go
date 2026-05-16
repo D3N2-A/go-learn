e040c37effc6d8a768efde91319e70dfd03c99cb
Anmol  Sharma <asharma@icoreconnect.com>
2026-05-17 00:58:47 +0530

    String util

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 
@@ -23,11 +24,26 @@ func main(){
 	age--
 
 	// Strings -> defined only by double quotes
+	// string in Go is a sequesnce of byte values
 	var name string = "String -> Hello, World!"
 	fmt.Println(name, len(name))
 	// Slice [:]
 	name = "Var reassign"
 	fmt.Println(name[:])
+	// Strings are immutable you can not update value but can reassign. Also they are reference type so so string is copied not its value. 
+	var first = "test1"
+	var second = first
+	second = "test2"
+	first = "123"
+	fmt.Println(first)
+	fmt.Println(second)
+
+
+	// strings utlity package
+	str3 := "aquickbrownfoxjumpedoveralazydog"
+	ct := strings.Count(str3, "ui")
+	fmt.Println(ct)
+	
 
 
 	fmt.Println("Hello, World!", age, age1)

a9ce7b0a9e835d6cfa83b81bcf4ec609bd680e88
Anmol  Sharma <asharma@icoreconnect.com>
2026-05-17 00:03:52 +0530

    String and var

--- hello.go
+++ hello.go
@@ -1,10 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 
 func main(){
+	// Declaring variable with datatype
 	var age int 
+	// Initializing that variable
 	age = 10
-	fmt.Println("Hello, World!", age)
+	// Walrus operator -> Auto infer type
+	age1 := 20
+	// Operators + can join strings
+	str1  := "Hello,"
+	str2  := "World!"
+	fmt.Println(str1+" "+str2)
+
+	// ++ -- -> incr or decr but can't prepend
+	// && || ! AND, OR, NOT
+	age++
+	age--
+
+	// Strings -> defined only by double quotes
+	var name string = "String -> Hello, World!"
+	fmt.Println(name, len(name))
+	// Slice [:]
+	name = "Var reassign"
+	fmt.Println(name[:])
+
+
+	fmt.Println("Hello, World!", age, age1)
 }

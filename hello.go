25b5819634e1a9562953af376af3291bd964fc01
Anmol  Sharma <asharma@icoreconnect.com>
2026-05-19 19:14:30 +0530

    Slice complete

--- hello.go
+++ hello.go
@@ -43,6 +43,35 @@ func main(){
 	str3 := "aquickbrownfoxjumpedoveralazydog"
 	ct := strings.Count(str3, "ui")
 	fmt.Println(ct)
+
+	// Array var varName [size]dataType
+	// Array var varName = [size]dataType{initialize with items or omit brackets for just declaration}
+	var fixed = [3]string{"Hello", " ", ", World!"}
+	var counted = [...]string{"Test"}
+	fmt.Println(fixed, counted)
+	// Array cannot be resized, you have to exlicitly define length of array. 
+	// This is a big limitation so we rarely use array, we use slizes
+	// Arrays are value types -> passing it to functions, returnging from a function creates a copy of original array
+
+
+	// Slices -> Data structure similar to array but can change size
+	// Slice is defined similar to array, you just omit sizze
+	// You can create empty slice using make -> Make returns type of arg not pointer to it (unlike new)
+
+	var mySlice = make([]string, 3)
+	mySlice[0] = "Hello"
+	mySlice[1] = " "
+	mySlice[2] = ", World!"
+	fmt.Println(mySlice)
+	mySlice = append(mySlice, " This is great")
+	fmt.Println(mySlice)
+	// You can create a slice with fixed size -> Good practice since extending slice requires allocating new memory and copying content
+	var newSlice = make([]string, 0, 10) // Initializes slice with 0 empty elements, memory allocation for 10 elements, for 10 empty element ommit size arg
+	newSlice[0] = "Hello, World!"
+	// You can create slice from array [:] operator
+
+
+
 	
 
 
